1f3437db50d433b33f9ad8ddde27858cfcfbc312
BarbosaShazam <nascimento.barbosa.felipe@escola.pr.gov.br>
2024-09-24 12:13:23 -0300

    4214124

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ function soma (num1, num2){return num1+num2;}
     
 function area (lado) {return lado*lado}
 let areaTraprezio=function(teste1,teste2,teste3){return teste1+teste2*teste3/2 }
+let media=function(aa,aa2,aa3){return aa+aa2+aa3/3}
