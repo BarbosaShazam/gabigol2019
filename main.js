090aa05ca1774fdc2ca3db3be79e1c13a2c14786
BarbosaShazam <nascimento.barbosa.felipe@escola.pr.gov.br>
2024-10-01 12:10:41 -0300

    55555555555

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
 let nome = ['Joao','Danilo','Alexandre','Henrique','Carlos','Renato','Cleiton','ana','maria','neide','Jesus','Cristo','Cristo','Lucia','Miguel','Martha','Darlan','Fernanda','Monteiro','Sanana'];
 let i = 0;
-while(i<nome.lenght){
+while(i<nome.length){
  {console.log(i)
  console.log (nome[i]);
  i++;}}
