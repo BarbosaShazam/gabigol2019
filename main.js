12a3bdd06f2a4d59ee4ce9795fa80d5cf549837a
BarbosaShazam <nascimento.barbosa.felipe@escola.pr.gov.br>
2024-10-01 11:57:31 -0300

    aaaa

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function soma (num1, num2){return num1+num2;}
+// function soma (num1, num2){return num1+num2;}
     
-function area (lado) {return lado*lado}
-let areaTraprezio=function(teste1,teste2,teste3){return teste1+teste2*teste3/2 }
-let media=function(aa,aa2,aa3){return aa+aa2+aa3/3};
+// function area (lado) {return lado*lado}
+// let areaTraprezio=function(teste1,teste2,teste3){return teste1+teste2*teste3/2 }
+// let media=function(aa,aa2,aa3){return aa+aa2+aa3/3};
+
+let nome = ['Joao','Danilo','Alexandre','Henrique','Carlos','Renato','Cleiton','ana','maria','neide','Jesus','Cristo','Cristo','Lucia','Miguel','Martha','Darlan','Fernanda','Monteiro','Sanana'];
+let i = 0;
+ {console.log(i)
+ console.log (nome[i]);
+ i=i+i;}
+ 
